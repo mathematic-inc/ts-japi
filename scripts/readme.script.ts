6da14dc2fe15f103846dfead9e899f4b98398496
jun-sheaf <34705014+jun-sheaf@users.noreply.github.com>
2020-07-15 19:35:16 +0200

    Fixed Documentation

--- scripts/readme.script.ts
+++ scripts/readme.script.ts
@@ -2,7 +2,7 @@ import { readFileSync, writeFileSync } from "fs";
 
 let readme = readFileSync("README.md", "utf-8")
  .replace(/\.md/g, ".html")
- .replace(/\`\]\(/g, "`](https://jun-sheaf.github.io/ts-japi/")
+ .replace(/\`\]\(/g, "`](https://mu-io.github.io/ts-japi/")
  .replace(`[ts-japi](README.html) › [Globals](globals.html)
 
 # ts-japi
