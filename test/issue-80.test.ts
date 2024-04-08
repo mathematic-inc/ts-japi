8a644cbf4ca8a44ffb993fc8ab7e77dd880d3c30
Edward Mackenzie <71255911+egmacke@users.noreply.github.com>
2024-04-08 17:14:04 +0100

    feat: serialize polymorphic documents (#81)
    * fix: fix logic where nested includes were not calculated correctly. Fixes #68
    
    * feat: serialize polymorphic documents

---
+++ test/issue-80.test.ts
@@ -0,0 +1,58 @@
+import { PolymorphicSerializer, Serializer } from '../lib';
+import Resource from '../lib/models/resource.model';
+
+describe('Issue #80 - Polymorphic serializer', () => {
+  abstract class Model {
+    public type: string;
+
+    constructor(public id: string) {}
+  }
+
+  class Model1 extends Model {
+    constructor(id: string, public model1: string) {
+      super(id);
+      this.type = 'type:Model1';
+    }
+  }
+
+  class Model2 extends Model {
+    constructor(id: string, public model2: string) {
+      super(id);
+      this.type = 'type:Model2';
+    }
+  }
+
+  class Model3 extends Model {
+    constructor(id: string, public model2: string) {
+      super(id);
+      this.type = 'type:Model3';
+    }
+  }
+
+  it('should work polymorphicly', async () => {
+    const model1: Model1 = new Model1('1', 'model1');
+    const model2: Model2 = new Model2('2', 'model2');
+    const model3: Model3 = new Model3('3', 'model3');
+
+    const Model1Serializer = new Serializer<Model1>('Model1');
+    const Model2Serializer = new Serializer<Model2>('Model2');
+
+    const PolySerializer = new PolymorphicSerializer<Model>('Model', 'type', {
+      'type:Model1': Model1Serializer,
+      'type:Model2': Model2Serializer,
+    });
+
+    const data = (await PolySerializer.serialize([model1, model2, model3])) as {
+      data: Resource<Model>;
+    };
+
+    expect(data.data).toBeInstanceOf(Array);
+    expect(data.data).toHaveLength(3);
+    expect(data.data[0].id).toEqual('1');
+    expect(data.data[0].type).toEqual('Model1');
+    expect(data.data[1].id).toEqual('2');
+    expect(data.data[1].type).toEqual('Model2');
+    expect(data.data[2].id).toEqual('3');
+    expect(data.data[2].type).toEqual('Model');
+  });
+});
